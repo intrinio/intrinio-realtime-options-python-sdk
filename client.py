1f953615b7d8fd2bbec0b6356df284f9e325c874
ssnyder <ssnyder@intrinio.com>
2023-04-14 19:05:15 -0500

    version bump

--- client.py
+++ client.py
@@ -738,7 +738,7 @@ class Client:
 
     def __try_set_token(self) -> bool:
         _log.info("Authorizing...")
-        headers = {"Client-Information": "IntrinioOptionsPythonSDKv2.1"}
+        headers = {"Client-Information": "IntrinioOptionsPythonSDKv2.2"}
         try:
             response: requests.Response = requests.get(self.__get_auth_url(), headers=headers, timeout=1)
             if response.status_code != 200:
