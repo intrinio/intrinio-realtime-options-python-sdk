7710db5013512c34713d0657b7c00863ccb8e4b3
ssnyder-intrinio <87659980+ssnyder-intrinio@users.noreply.github.com>
2024-07-10 18:20:26 -0500

    Token refresh and Delay feed (#8)
    * delayed
    
    * always refresh token

--- client.py
+++ client.py
@@ -321,9 +321,8 @@ class _WebSocket(websocket.WebSocketApp):
         else:
             with self.__wsLock:
                 self.__is_reconnecting = True
-            if (time.time() - self.__last_reset > (60 * 60 * 24 * 5)):
-                token: str = self.__get_token()
-                super().url = self.__get_url(token)
+            token: str = self.__get_token(None)
+            super().url = self.__get_url(token)
             self.start()
             return False
 
@@ -372,13 +371,14 @@ class _WebSocket(websocket.WebSocketApp):
 
 class Config:
     def __init__(self, api_key: str, provider: Providers, num_threads: int = 4, log_level: LogLevel = LogLevel.INFO,
-                 manual_ip_address: str = None, symbols: set[str] = None):
+                 manual_ip_address: str = None, symbols: set[str] = None, delayed: bool = False):
         self.api_key: str = api_key
         self.provider: Providers = provider
         self.num_threads: int = num_threads
         self.manual_ip_address: str = manual_ip_address
         self.symbols: list[str] = symbols
         self.log_level: LogLevel = log_level
+        self.delayed: bool = delayed
 
 def _transform_contract_to_new(contract: str) -> str:
     if (len(contract) <= 9) or (contract.find('.') >= 9):
@@ -623,6 +623,10 @@ class Client:
                 raise ValueError("Parameter 'on_unusual_activity' must be a function")
         else:
             self.__use_on_unusual_activity: bool = False
+        if (not config.delayed) or (not isinstance(config.delayed, bool)):
+            self.__delayed: bool = False
+        else:
+            self.__delayed: bool = config.delayed
         self.__provider: Providers = config.provider
         self.__apiKey: str = config.api_key
         self.__manualIP: str = config.manual_ip_address
@@ -664,10 +668,11 @@ class Client:
             raise ValueError("Provider not specified")
 
     def __get_web_socket_url(self, token: str) -> str:
+        delay: str = "&delayed=true" if self.__delayed else ""
         if self.__provider == Providers.OPRA:
-            return "wss://realtime-options.intrinio.com/socket/websocket?vsn=1.0.0&token=" + token
+            return "wss://realtime-options.intrinio.com/socket/websocket?vsn=1.0.0&token=" + token + delay
         elif self.__provider == Providers.MANUAL:
-            return "ws://" + self.__manualIP + "/socket/websocket?vsn=1.0.0&token=" + token
+            return "ws://" + self.__manualIP + "/socket/websocket?vsn=1.0.0&token=" + token + delay
         else:
             raise ValueError("Provider not specified")
 
--- example_app.py
+++ example_app.py
@@ -75,7 +75,7 @@ class Summarize(threading.Thread):
 
     def run(self):
         while not self.__stop_flag.is_set():
-            time.sleep(10.0)
+            time.sleep(30.0)
             (dataMsgs, txtMsgs, queueDepth) = self.__client.get_stats()
             client.log("Client Stats - Data Messages: {0}, Text Messages: {1}, Queue Depth: {2}".format(dataMsgs, txtMsgs, queueDepth))
             client.log(
@@ -92,28 +92,16 @@ class Summarize(threading.Thread):
 
 # Your config object MUST include the 'api_key' and 'provider', at a minimum
 config: client.Config = client.Config(
-    api_key="",
+    api_key="API_KEY_HERE",
     provider=client.Providers.OPRA,
     num_threads=8,
-    symbols=["AAPL", "BRKB__230217C00300000"],
-    # this is a static list of symbols (options contracts or option chains) that will automatically be subscribed to when the client starts
-    log_level=client.LogLevel.INFO)
+    symbols=["AAPL", "BRKB__230217C00300000"], # this is a static list of symbols (options contracts or option chains) that will automatically be subscribed to when the client starts
+    log_level=client.LogLevel.INFO,
+    delayed=False) #set delayed parameter to true if you have realtime access but want the data delayed 15 minutes anyway
 
 # Register only the callbacks that you want.
 # Take special care when registering the 'on_quote' handler as it will increase throughput by ~10x
 intrinioRealtimeOptionsClient: client.Client = client.Client(config, on_trade=on_trade, on_quote=on_quote, on_refresh=on_refresh, on_unusual_activity=on_unusual_activity)
-
-# Use this to subscribe to the entire universe of symbols (option contracts). This requires special permission.
-# intrinioRealtimeOptionsClient.join_firehose()
-
-# Use this to subscribe, dynamically, to an option chain (all option contracts for a given underlying contract).
-# intrinioRealtimeOptionsClient.join("AAPL")
-
-# Use this to subscribe, dynamically, to a specific option contract.
-# intrinioRealtimeOptionsClient.join("AAP___230616P00250000")
-
-# Use this to subscribe, dynamically, a list of specific option contracts or option chains.
-# intrinioRealtimeOptionsClient.join("GOOG__220408C02870000", "MSFT__220408C00315000", "AAPL__220414C00180000", "TSLA", "GE")
 
 stop_event = Event()
 
@@ -131,7 +119,21 @@ summarize_thread = Summarize(stop_event, intrinioRealtimeOptionsClient)
 summarize_thread.start()
 
 intrinioRealtimeOptionsClient.start()
+
+#use this to join the channels already declared in your config
 intrinioRealtimeOptionsClient.join()
+
+# Use this to subscribe to the entire universe of symbols (option contracts). This requires special permission.
+# intrinioRealtimeOptionsClient.join_firehose()
+
+# Use this to subscribe, dynamically, to an option chain (all option contracts for a given underlying contract).
+# intrinioRealtimeOptionsClient.join("AAPL")
+
+# Use this to subscribe, dynamically, to a specific option contract.
+# intrinioRealtimeOptionsClient.join("AAP___230616P00250000")
+
+# Use this to subscribe, dynamically, a list of specific option contracts or option chains.
+# intrinioRealtimeOptionsClient.join("GOOG__220408C02870000", "MSFT__220408C00315000", "AAPL__220414C00180000", "TSLA", "GE")
 
 time.sleep(60 * 60)
 # sigint, or ctrl+c, during the thread wait will also perform the same below code.
